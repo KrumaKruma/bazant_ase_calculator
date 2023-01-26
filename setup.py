f68064fe76339c1e5fe82b2bf1fb3a9f079b6ad5
Marco Krummenacher <marco.krummenacher@unibas.ch>
2023-01-26 10:37:20 +0100

    updated setup file

--- setup.py
+++ setup.py
@@ -17,8 +17,7 @@ __url__ = "https://github.com/KrumaKruma/bazant_ase_calculator"
 FORTRAN = "f90"
 
 # GNU (default)
-COMPILER_FLAGS = ["-O3", "-fopenmp", "-m64", "-march=native", "-fPIC",
-                    "-Wno-maybe-uninitialized", "-Wno-unused-function", "-Wno-cpp"]
+COMPILER_FLAGS = ["-O3", "-fopenmp", "-m64"]
 LINKER_FLAGS = ["-lgomp"]
 MATH_LINKER_FLAGS = ["-lblas", "-llapack"]
 
@@ -29,13 +28,13 @@ if mkl_exists(verbose=True):
 
 # # For clang without OpenMP: (i.e. most Apple/mac system)
 if sys.platform == "darwin" and all(["gnu" not in arg for arg in sys.argv]):
-    COMPILER_FLAGS = ["-O3", "-m64", "-march=native", "-fPIC"]
+    COMPILER_FLAGS = ["-O3", "-m64"]
     LINKER_FLAGS = []
     MATH_LINKER_FLAGS = ["-lblas", "-llapack"]
 
 # # Intel
 if any(["intelem" in arg for arg in sys.argv]):
-    COMPILER_FLAGS = ["-xHost", "-O3", "-axAVX", "-qopenmp"]
+    COMPILER_FLAGS = ["-O3", "-qopenmp"]
     LINKER_FLAGS = ["-liomp5", " -lpthread", "-lm", "-ldl"]
     MATH_LINKER_FLAGS = ["-L${MKLROOT}/lib/intel64", "-lmkl_rt"]
 
